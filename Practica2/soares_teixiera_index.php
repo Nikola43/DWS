d1ced0beaab72657d0e306e26cb617b172101842
Paulo Soares <pauloxti@gmail.com>
2018-09-20 14:47:06 +0200

    practica php

---
+++ Practica2/soares_teixiera_index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Practica2</title>
+</head>
+<body>
+<?php
+echo "<h1>Practica 2</h1>\n";
+echo "<p>Éste código HTML ha sido generado mediante instrucciones escritas en lenguaje PHP.</p>\n";
+echo "<hr/>\n";
+?>
+</body>
+</html>
